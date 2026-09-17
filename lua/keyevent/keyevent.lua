0b733816311d4ecbab791c37b9e85c013eab0eb2
OGAWA Keiji <70509917+kibi2@users.noreply.github.com>
2026-09-17 14:36:29 +0900

    feat: new keyevent member

--- lua/keyevent/keyevent.lua
+++ lua/keyevent/keyevent.lua
@@ -3,39 +3,33 @@ local log = require("keyevent.log")
 
 local M = {}
 
---------------------------------------------------
--- default interval (ms)
---------------------------------------------------
-
 ---@enum KeyEventSource
-local KEY_EVENT_SOURCE = {
-    UNDEFINED = "undefined",
-    ON_KEY = "on_key",
-    KEYMAP = "keymap",
+M.KEY_EVENT_SOURCE = {
+	UNDEFINED = "undefined",
+	ON_KEY = "on_key",
+	KEYMAP = "keymap",
 }
 
 ---@enum KeyEventType
-local KEY_EVENT = {
-    CLICK = "click",
-    TAP = "tap",
-    REPEAT = "repeat",
+M.KEY_EVENT = {
+	CLICK = "click",
+	TAP = "tap",
+	REPEAT = "repeat",
 }
 
----@enum RawEvent
-local RAW_EVENT = {
-    CLICK = "click",
-    TAP = "tap",
-    HOLD_START = "hold_start",
-    HOLD_REPEAT = "hold_repeat",
+---@enum KeyEventState
+local STATE = {
+	HOLD = "hold",
+	NO_HOLD = "no_hold",
 }
 
 ---@class KeyEvent
 ---@field source KeyEventSource
 ---@field type KeyEventType
+---@field ng_repeat boolean
 ---@field key string
----@field time integer
 ---@field prev_key string
----@field vim_count integer
+---@field time integer
 ---@field interval integer
 ---@field nt integer
 ---@field nr integer
@@ -43,110 +37,154 @@ local RAW_EVENT = {
 
 ---@return integer
 local function get_time()
-    return math.floor(vim.loop.hrtime() / 1e6)
+	return math.floor(vim.loop.hrtime() / 1e6)
 end
 
+---@type KeyEvent
 local default_event = {
-    source = KEY_EVENT_SOURCE.UNDEFINED,
-    type = KEY_EVENT.CLICK,
-    key = "",
-    prev_key = "",
-    time = get_time(),
-    interval = 0,
-    vim_count = 0,
-    nt = 0,
-    nr = 0,
-    hold_start = 0,
+	source = M.KEY_EVENT_SOURCE.UNDEFINED,
+	type = M.KEY_EVENT.CLICK,
+	ng_repeat = false,
+	key = "",
+	prev_key = "",
+	time = get_time(),
+	interval = 0,
+	nt = 0,
+	nr = 0,
+	hold_start = 0,
 }
 ---@type KeyEvent
 local prev_event = vim.deepcopy(default_event)
+---@type KeyEventState
+local state = STATE.NO_HOLD
 
 ---@param event KeyEvent
----@return RawEvent
-local function get_raw_event(event)
-    if event.key ~= event.prev_key then
-        return RAW_EVENT.CLICK
-    elseif threshold.is_delay(event.interval) then
-        return RAW_EVENT.HOLD_START
-    elseif threshold.is_interval(event.interval) and event.nr >= 2 then
-        return RAW_EVENT.HOLD_REPEAT
-    elseif threshold.is_tap(event.interval) then
-        return RAW_EVENT.TAP
-    else
-        return RAW_EVENT.CLICK
-    end
+local function process_no_hold(event)
+	if threshold.is_tap(event.interval) then
+		event.type = M.KEY_EVENT.TAP
+	else
+		event.type = M.KEY_EVENT.CLICK
+	end
+	event.ng_repeat = threshold.is_repeat(event.interval)
+	if M.is_same_key(event) and event.type == M.KEY_EVENT.TAP then
+		event.nt = event.nt + 1
+	else
+		event.nt = 0
+	end
+	event.nr = 0
+	event.hold_start = 0
 end
 
 ---@param event KeyEvent
-local function set_event_type(event)
-    local raw_event = get_raw_event(event)
-    if raw_event == RAW_EVENT.CLICK then
-        event.type = KEY_EVENT.CLICK
-        event.nt = 1
-        event.nr = 0
-        event.vim_count = vim.v.count
-    elseif raw_event == RAW_EVENT.HOLD_START then
-        event.type = KEY_EVENT.REPEAT
-        event.nr = 2
-        event.hold_start = prev_event.time
-    elseif raw_event == RAW_EVENT.HOLD_REPEAT then
-        event.type = KEY_EVENT.REPEAT
-        event.nr = event.nr + 1
-    elseif raw_event == RAW_EVENT.TAP then
-        event.type = KEY_EVENT.TAP
-        event.nt = event.nt + 1
-        event.nr = 0
-        event.vim_count = vim.v.count
-    else
-        assert(false, "")
-    end
+local function process_hold(event)
+	event.type = M.KEY_EVENT.REPEAT
+	event.ng_repeat = false
+	if event.nr == 0 then
+		event.hold_start = prev_event.time
+	end
+	event.nr = event.nr + 1
+end
+
+local function transition(event)
+	if state == STATE.NO_HOLD then
+		if M.is_same_key(event) and threshold.is_hold(event.interval) then
+			state = STATE.HOLD
+		end
+	else
+		if
+			not (M.is_same_key(event) and threshold.is_repeat(event.interval))
+		then
+			state = STATE.NO_HOLD
+		end
+	end
+end
+
+---@param event KeyEvent
+local function process_event(event)
+	transition(event)
+	if state == STATE.NO_HOLD then
+		process_no_hold(event)
+	else
+		process_hold(event)
+	end
 end
 
 ---@param source KeyEventSource
 ---@param typed string
 ---@return KeyEvent
 local function get_event(source, typed)
-    local event = vim.deepcopy(prev_event)
-    event.source = source
-    event.key = typed
-    event.prev_key = prev_event.key
-    event.time = get_time()
-    event.interval = event.time - prev_event.time
-    set_event_type(event)
-    prev_event = vim.deepcopy(event)
-    log.debug(
-        "%s\t%s\t:(%s) %d, [%d %d]\t[%d]",
-        event.source,
-        event.type,
-        event.key,
-        event.vim_count,
-        event.nt,
-        event.nr,
-        event.interval
-    )
-    return event
+	local event = vim.deepcopy(prev_event)
+	event.source = source
+	event.key = typed
+	event.prev_key = prev_event.key
+	event.time = get_time()
+	event.interval = event.time - prev_event.time
+	process_event(event)
+	return event
+end
+
+---@param event KeyEvent
+local function debug_event(event)
+	log.debug(
+		"%s\t%s\t:(%s, %s), [%d %d]\t%s [%d, %d]",
+		event.source,
+		event.type,
+		event.prev_key,
+		event.key,
+		event.nt,
+		event.nr,
+		event.ng_repeat and "NG" or "",
+		event.interval,
+		M.hold_time(event)
+	)
 end
 
 ---@param typed string
 local function on_key_event(typed)
-    if prev_event.source == KEY_EVENT_SOURCE.KEYMAP and typed == prev_event.key then
-        return
-    end
-    get_event(KEY_EVENT_SOURCE.ON_KEY, typed)
+	if
+		prev_event.source == M.KEY_EVENT_SOURCE.KEYMAP
+		and typed == prev_event.key
+	then
+		return
+	end
+	local event = get_event(M.KEY_EVENT_SOURCE.ON_KEY, typed)
+	if event.interval <= 10 then
+		return
+	end
+	prev_event = vim.deepcopy(event)
+	debug_event(event)
 end
 
 ---@param typed string
 local function on_key(_, typed)
-    if #typed == 0 then
-        return
-    end
-    on_key_event(typed)
+	if #typed == 0 then
+		return
+	end
+	on_key_event(typed)
 end
 
 ---@param typed string
 ---@return KeyEvent
 function M.keymap_event(typed)
-    return get_event(KEY_EVENT_SOURCE.KEYMAP, typed)
+	local event = get_event(M.KEY_EVENT_SOURCE.KEYMAP, typed)
+	prev_event = vim.deepcopy(event)
+	debug_event(event)
+	return event
+end
+
+---@param event KeyEvent
+---@return boolean
+function M.is_same_key(event)
+	return event.key == event.prev_key
+end
+
+---@param event KeyEvent
+---@return integer
+function M.hold_time(event)
+	if event.type ~= M.KEY_EVENT.REPEAT then
+		return 0
+	end
+	return event.time - event.hold_start
 end
 
 vim.on_key(on_key)
--- lua/keyevent/threshold.lua
+++ lua/keyevent/threshold.lua
@@ -6,33 +6,30 @@ local M = {}
 
 ---@return integer
 local function get_delay()
-    return os.delay or config.threshold.delay
+	return os.delay or config.threshold.delay
 end
 
 ---@return integer
 local function get_interval()
-    return os.interval or config.threshold.interval
+	return os.interval or config.threshold.interval
 end
 
 ---@param interval integer
 ---@return boolean
-function M.is_delay(interval)
-    log.probe(get_delay())
-    log.probe(interval)
-    log.probe(config.threshold.delta)
-    return math.abs(get_delay() - interval) <= config.threshold.delta
+function M.is_hold(interval)
+	return math.abs(get_delay() - interval) <= config.threshold.delta
 end
 
 ---@param interval integer
 ---@return boolean
-function M.is_interval(interval)
-    return math.abs(get_interval() - interval) <= config.threshold.delta
+function M.is_repeat(interval)
+	return math.abs(get_interval() - interval) <= config.threshold.delta
 end
 
 ---@param interval integer
 ---@return boolean
 function M.is_tap(interval)
-    return interval <= config.threshold.tap
+	return interval <= config.threshold.tap
 end
 
 return M
