56a1c03b36d134ac143c1c9bd402950588c756d3
OGAWA Keiji <70509917+kibi2@users.noreply.github.com>
2026-09-24 09:32:00 +0900

    feat: histgram overlap

--- lua/keyevent/histgram.lua
+++ lua/keyevent/histgram.lua
@@ -6,14 +6,28 @@ local M = {}
 
 ---@class Histgram
 ---@field bin integer
-----@field bins table<integer, HistgramBin>
+---@field overlap boolean
+---@field count integer
 ---@field bins HistgramBin[]
 
+---@param hist Histgram
+---@return integer[]
+local function get_indexes(hist)
+	local indexes = {}
+	for index in pairs(hist.bins) do
+		indexes[#indexes + 1] = index
+	end
+	table.sort(indexes)
+	return indexes
+end
+
 ---@param bin integer
+---@param overlap? boolean
 ---@return Histgram
-function M.new(bin)
+function M.new(bin, overlap)
 	return {
 		bin = bin,
+		overlap = overlap or false,
 		count = 0,
 		bins = {},
 	}
@@ -22,6 +36,7 @@ end
 ---@param hist Histgram
 ---@param value number
 function M.add(hist, value)
+	hist.count = hist.count + 1
 	local index = math.floor(value / hist.bin)
 	local bin = hist.bins[index]
 	if not bin then
@@ -33,24 +48,30 @@ function M.add(hist, value)
 	end
 	bin.count = bin.count + 1
 	bin.total = bin.total + value
+	if hist.overlap then
+		local index = math.floor((value - hist.bin / 2) / hist.bin)
+		local bin = hist.bins[index]
+		if not bin then
+			bin = {
+				count = 0,
+				total = 0,
+			}
+			hist.bins[index] = bin
+		end
+		bin.count = bin.count + 1
+		bin.total = bin.total + value
+	end
 end
 
 ---@param hist Histgram
 ---@return number
 function M.median_average(hist)
-	if next(hist.bins) == nil then
+	if hist.count == 0 then
 		return math.huge
 	end
-	local indexes = {}
-	local ntotal = 0
-	for index, bin in pairs(hist.bins) do
-		indexes[#indexes + 1] = index
-		ntotal = ntotal + bin.count
-	end
-	table.sort(indexes)
-	local middle = math.ceil(ntotal / 2)
+	local middle = math.ceil(hist.count / 2)
 	local count = 0
-	for _, index in ipairs(indexes) do
+	for _, index in ipairs(get_indexes(hist)) do
 		local bin = hist.bins[index]
 		count = count + bin.count
 		if count >= middle then
@@ -58,6 +79,52 @@ function M.median_average(hist)
 		end
 	end
 	return math.huge
+end
+
+---@param hist Histgram
+---@return number
+function M.mode_ratio(hist)
+	if hist.count == 0 then
+		return 0
+	end
+
+	local max_count = 0
+	for _, bin in pairs(hist.bins) do
+		max_count = math.max(max_count, bin.count)
+	end
+	return max_count / hist.count
+end
+
+---@param hist Histgram
+---@return number
+function M.mode_ave(hist)
+	if hist.count == 0 then
+		return math.huge
+	end
+	local mode
+	for _, bin in pairs(hist.bins) do
+		if not mode or bin.count > mode.count then
+			mode = bin
+		end
+	end
+	if not mode then
+		return math.huge
+	end
+	return mode.total / mode.count
+end
+
+---@param hist Histgram
+---@return string
+function M.to_string(hist)
+	local ave = {}
+	local count = {}
+	for _, index in ipairs(get_indexes(hist)) do
+		local bin = hist.bins[index]
+		ave[#ave + 1] = string.format("%4d", math.floor(bin.total / bin.count))
+		count[#count + 1] = string.format("%4d", bin.count)
+	end
+
+	return table.concat(ave) .. "\n" .. table.concat(count)
 end
 
 return M
